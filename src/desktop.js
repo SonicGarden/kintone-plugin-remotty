cc7aa4ff37baab4c1f84bf3f9490e326efff4567
Toru KAWAMURA <tkawa@4bit.net>
2018-02-15 10:39:29 +0900

    更新時差分のみを通知するように変更

--- src/desktop.js
+++ src/desktop.js
@@ -12,18 +12,31 @@ jQuery.noConflict();
   };
 
   const setSubmitNotificationEvent = () => {
-    const submitEvents = [
+    window.kintonePluginRemotty = {};
+
+    const editShowEvents = [
+      "app.record.index.edit.show",
+      "app.record.edit.show"
+    ];
+    kintone.events.on(editShowEvents, (e) => {
+      console.log(e);
+      setOriginalRecord(e.appId, e.recordId, e.record);
+    });
+    const submitSuccessEvents = [
       "app.record.index.edit.submit.success",
       "app.record.edit.submit.success",
       "app.record.create.submit.success"
     ];
-    kintone.events.on(submitEvents, (e) => {
+    kintone.events.on(submitSuccessEvents, (e) => {
+      console.log(e);
       kintone.Promise.all([appPromise(e.appId), formPromise(e.appId)]).then(([app, form]) => {
+        const originalRecord = getOriginalRecord(e.appId, e.recordId);
+        setOriginalRecord(e.appId, e.recordId, e.record);
         const eventString = (e.type.indexOf('create') !== -1) ? "作成" : "更新";
         const recordUrl = `${window.location.protocol}//${window.location.host}/k/${e.appId}/show#record=${e.recordId}`;
         const THREE_BACKTICKS = "```";
         const payload = {
-          message: `kintone ${app.name} (appId:${e.appId}) のレコード ${e.recordId} が${eventString}されました\n${recordUrl}\n${THREE_BACKTICKS}${recordToText(e.record, form)}${THREE_BACKTICKS}`
+          message: `kintone ${app.name} (appId:${e.appId}) のレコード ${e.recordId} が${eventString}されました\n${recordUrl}\n${THREE_BACKTICKS}${recordToText(e.record, originalRecord, form)}${THREE_BACKTICKS}`
         };
         return kintone.proxy(webhookUrl, 'POST', headers, payload);
       }).then(([body, status, headers]) => {
@@ -34,11 +47,13 @@ jQuery.noConflict();
     });
   };
 
-  const recordToText = (record, form) => {
+  const recordToText = (record, originalRecord, form) => {
     const values = Object.keys(record).reduce((obj, key) => {
-      const formProperty = form.properties.find((property) => property.code === key);
-      const label = (formProperty && formProperty.label) || key;
-      obj[label] = record[key].value;
+      if (originalRecord[key] == null || JSON.stringify(record[key].value) !== JSON.stringify(originalRecord[key].value)) {
+        const formProperty = form.properties.find((property) => property.code === key);
+        const label = (formProperty && formProperty.label) || key;
+        obj[label] = record[key].value;
+      }
       return obj;
     }, {});
     return json2yaml.stringify(values);
@@ -57,6 +72,14 @@ jQuery.noConflict();
       forms[appId] = kintone.api(kintone.api.url("/k/v1/form", true), 'GET', { app: appId });
     }
     return forms[appId];
+  }
+  const getOriginalRecord = (appId, recordId) => (
+    (window.kintonePluginRemotty != null ? window.kintonePluginRemotty[`${appId}/${recordId}`] : null) || {}
+  )
+  const setOriginalRecord = (appId, recordId, record) => {
+    window.kintonePluginRemotty = {
+      [`${appId}/${recordId}`]: record
+    };
   }
 
   setSubmitNotificationEvent();
